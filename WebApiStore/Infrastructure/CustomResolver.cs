2cd43830a32326162c344b648bc9f686949fb7d5
knolegsb <seon2you@hotmail.com>
2015-11-12 13:46:38 -0800

    added custom resolver

---
+++ WebApiStore/Infrastructure/CustomResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using WebApiStore.Models;
+
+namespace WebApiStore.Infrastructure
+{
+    public class CustomResolver : IDependencyResolver, IDependencyScope
+    {                       
+        public object GetService(Type serviceType)
+        {
+            return serviceType == typeof(IRepository) ? new ProductRepository() : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Enumerable.Empty<object>();
+        }
+        
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
