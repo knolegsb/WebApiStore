b47768e8503c0e8c05cb167e89c7587e0fbcc070
knolegsb <seon2you@hotmail.com>
2015-11-12 13:59:17 -0800

    non-rest web service

--- WebApiStore/Models/OrderLine.cs
+++ WebApiStore/Models/OrderLine.cs
@@ -1,14 +1,24 @@
-﻿namespace WebApiStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+
+namespace WebApiStore.Models
 {
     public class OrderLine
     {
+        [HttpBindNever]
         public int Id { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int Count { get; set; }
 
+        [Required]
         public int ProductId { get; set; }
+        [HttpBindNever]
         public int OrderId { get; set; }
 
+        [HttpBindNever]
         public Product Product { get; set; }
+        [HttpBindNever]
         public Order Order { get; set; }
 
     }
