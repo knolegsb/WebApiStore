c6b89755309bdd3b6a840b41dc093d39a5783658
knolegsb <seon2you@hotmail.com>
2015-11-12 01:08:54 -0800

    added product controller

---
+++ WebApiStore/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApiStore.Models;
+
+namespace WebApiStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private IRepository Repository { get; set; }
+        public ProductsController()
+        {
+            Repository = new ProductRepository();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return Repository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return Repository.Products.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public async Task PostProduct(Product product)
+        {
+            await Repository.SaveProductAsync(product);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await Repository.DeleteProductAsync(id);
+        }
+    }
+}
