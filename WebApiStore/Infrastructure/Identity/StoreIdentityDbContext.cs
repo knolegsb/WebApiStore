2c68a929b4cc1d8a94e4a3dc2f604721a1b99a32
knolegsb <seon2you@hotmail.com>
2015-11-11 23:59:53 -0800

    added identity

---
+++ WebApiStore/Infrastructure/Identity/StoreIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApiStore.Infrastructure.Identity
+{
+    public class StoreIdentityDbContext : IdentityDbContext<StoreUser>
+    {
+        public StoreIdentityDbContext() : base("SportsStoreIdentityDb")
+        {
+            Database.SetInitializer<StoreIdentityDbContext>(new StoreIdentityDbInitializer());
+        }
+
+        public static StoreIdentityDbContext Create()
+        {
+            return new StoreIdentityDbContext();
+        }
+    }
+}
